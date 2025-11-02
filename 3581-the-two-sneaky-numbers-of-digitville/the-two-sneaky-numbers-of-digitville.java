df20732a4cef8a7af54fbc2174d57c66649ad47e
jananimohan2175-git <jananimohan2175@gmail.com>
2025-11-02 20:24:52 +0530

    Time: 1 ms (99.99%) | Memory: 45.5 MB (6.96%) - LeetSync

---
+++ 3581-the-two-sneaky-numbers-of-digitville/the-two-sneaky-numbers-of-digitville.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] getSneakyNumbers(int[] nums) {
+     int n = nums.length;
+     int[] count = new int[n];
+     int[] ans = new int[2];   
+     int index=0;
+     for(int num : nums)
+     {
+        count[num]++;
+     }
+     for(int i=0;i<n;i++)
+     {
+        if(count[i]==2)
+        {
+            ans[index++]=i;
+        }
+     }
+     return ans;
+    }
+}
