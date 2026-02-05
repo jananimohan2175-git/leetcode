4c1828546c4bc6e3fc535daba7e5f2fe5d13b6b7
jananimohan2175-git <jananimohan2175@gmail.com>
2026-02-05 14:43:46 +0530

    Time: 1 ms (98.72%) | Memory: 46.5 MB (93.27%) - LeetSync

---
+++ 3651-transformed-array/transformed-array.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int[] constructTransformedArray(int[] nums) {
+        int n = nums.length;
+        int[] result = new int[n];
+        for(int i =0;i<n;i++)
+        {
+            int target = ((i +nums[i]) % n + n)%n;
+            result[i] = nums[target];
+        }
+        return result;
+    }
+}
